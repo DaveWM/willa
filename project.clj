90abbef1f92b17672364464b1ede4b41e90668e4
David Martin <david.martin@fundingcircle.com>
2019-01-23 19:08:46 +0000

    updated license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject willa "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A Clojure DSL for Kafka Streams"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "GPL V3"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [fundingcircle/jackdaw "0.4.2"]
                  [aysylu/loom "1.0.2"]
