81cedb20e784a47fbf140c18ed32ccf4867e9e6c
David Martin <david.martin@fundingcircle.com>
2019-02-05 11:27:43 +0000

    added a way of overriding how an entity is built

--- src/willa/core.clj
+++ src/willa/core.clj
@@ -16,10 +16,10 @@
   (streams/kstream builder entity))
 
 (defmethod entity->kstream :kstream [_ entity]
-  (:kstream entity))
+  (::kstreams-object entity))
 
 (defmethod entity->kstream :ktable [_ entity]
-  (-> (ws/coerce-to-kstream (:ktable entity))
+  (-> (ws/coerce-to-kstream (::kstreams-object entity))
       (streams/map (fn [[k v]]
                      [(if (instance? Windowed k) (.key k) k)
                       v]))))
@@ -32,10 +32,10 @@
   (streams/ktable builder entity))
 
 (defmethod entity->ktable :kstream [_ entity]
-  (ws/coerce-to-ktable (:kstream entity)))
+  (ws/coerce-to-ktable (::kstreams-object entity)))
 
 (defmethod entity->ktable :ktable [_ entity]
-  (:ktable entity))
+  (::kstreams-object entity))
 
 
 (defmulti ->joinable (fn [builder entity]
@@ -66,45 +66,57 @@
        (apply ws/join join-config)))
 
 
-(defmulti build-entity (fn [entity builder parents entities joins]
-                         (::entity-type entity)))
+(defmulti build-kstreams-object (fn [entity builder parents entities joins]
+                                  (::entity-type entity)))
 
 
-(defmethod build-entity :topic [entity builder parents entities _]
+(defmethod build-kstreams-object :topic [entity builder parents entities _]
   (doseq [p (map entities parents)]
     (streams/to (entity->kstream builder p) entity))
-  entity)
+  nil)
 
 
-(defmethod build-entity :kstream [entity builder parents entities joins]
+(defmethod build-kstreams-object :kstream [entity builder parents entities joins]
   (let [kstream (if (wu/single-elem? parents)
                   (entity->kstream builder (get entities (first parents)))
                   (-> (join-entities builder (get-join joins parents) entities)
                       ws/coerce-to-kstream))]
-    (assoc entity :kstream (cond-> kstream
-                                   (::xform entity) (ws/transduce-stream (::xform entity))))))
+    (cond-> kstream
+            (::xform entity) (ws/transduce-stream (::xform entity)))))
 
 
-(defmethod build-entity :ktable [entity builder parents entities joins]
+(defmethod build-kstreams-object :ktable [entity builder parents entities joins]
   (let [ktable-or-kstream (if (wu/single-elem? parents)
                             (->groupable builder (get entities (first parents)))
                             (join-entities builder (get-join joins parents) entities))]
-    (assoc entity :ktable (cond-> ktable-or-kstream
-                                  (::window entity) (ws/coerce-to-kstream)
-                                  (::group-by-fn entity) (streams/group-by (::group-by-fn entity) ws/default-serdes)
-                                  (::window entity) (ws/window-by (::window entity))
-                                  (::aggregate-adder-fn entity) (ws/aggregate (::aggregate-initial-value entity)
-                                                                              (::aggregate-adder-fn entity)
-                                                                              (::aggregate-subtractor-fn entity))
-                                  (::suppression entity) (ws/suppress (::suppression entity))
-                                  true (ws/coerce-to-ktable)))))
+    (cond-> ktable-or-kstream
+            (::window entity) (ws/coerce-to-kstream)
+            (::group-by-fn entity) (streams/group-by (::group-by-fn entity) ws/default-serdes)
+            (::window entity) (ws/window-by (::window entity))
+            (::aggregate-adder-fn entity) (ws/aggregate (::aggregate-initial-value entity)
+                                                        (::aggregate-adder-fn entity)
+                                                        (::aggregate-subtractor-fn entity))
+            (::suppression entity) (ws/suppress (::suppression entity))
+            true (ws/coerce-to-ktable))))
 
 
-(defn build-workflow! [builder {:keys [workflow entities joins]}]
+
+(defn- build-workflow!* [builder {:keys [workflow entities joins]} overrides]
   (let [g (apply l/digraph workflow)]
     (->> g
          (lalg/topsort)
          (map (juxt identity (partial l/predecessors g)))
          (reduce (fn [built-entities [node parents]]
-                   (update built-entities node build-entity builder parents built-entities joins))
+                   (let [build-fn (get overrides node build-kstreams-object)]
+                     (update built-entities node
+                             (fn [entity]
+                               (->> (build-fn entity builder parents built-entities joins)
+                                    (assoc entity ::kstreams-object))))))
                  entities))))
+
+
+(def build-workflow-unsafe! build-workflow!*)
+
+
+(defn build-workflow! [builder world]
+  (build-workflow!* builder world {}))
