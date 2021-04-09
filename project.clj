5ef0f18485b8a733304306a03cd8fc5f665e565f
Dave Martin <mail@davemartin.me>
2021-04-09 09:54:42 +0100

    Bump dep versions

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "https://github.com/DaveWM/willa"
   :license {:name "GPL V3"
             :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [fundingcircle/jackdaw "0.6.6"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [fundingcircle/jackdaw "0.7.8"]
                  [aysylu/loom "1.0.2"]
                  [rhizome "0.2.9"]
-                 [org.clojure/math.combinatorics "0.1.4"]]
+                 [org.clojure/math.combinatorics "0.1.6"]]
   :repl-options {:init-ns willa.core}
   :repositories [["confluent" "https://packages.confluent.io/maven/"]
                  ["clojars" "https://clojars.org/repo/"]]
-  :profiles {:test {:dependencies [[org.apache.kafka/kafka-streams-test-utils "2.3.0"]
+  :profiles {:test {:dependencies [[org.apache.kafka/kafka-streams-test-utils "2.3.1"]
                                    [log4j/log4j "1.2.17"]]}
-             :dev {:dependencies [[org.clojure/test.check "0.9.0"]]}
+             :dev {:dependencies [[org.clojure/test.check "1.1.0"]]}
              :ci {:jvm-opts ["-Djava.awt.headless=true"]}})
